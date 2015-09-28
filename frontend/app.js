7388bfa144a814624999f68c2520e81912adcbd5
Tanel Lebedev <tanel.lebedev@gmail.com>
2015-09-28 15:54:31 +0300

    use real data for charting

--- frontend/app.js
+++ frontend/app.js
@@ -37,29 +37,40 @@ $(function () {
         $(".total_saves").text(window.stats.total.saves);
         $(".total_kills").text(window.stats.total.kills);
 
+        if (!window.stats.history) {
+            window.stats.history = {};
+        }
+
         var data = {
-            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            labels: [],
             datasets: [
                 {
-                    label: "My First dataset",
+                    label: "Saves",
                     fillColor: "rgba(220,220,220,0.5)",
                     strokeColor: "rgba(220,220,220,0.8)",
                     highlightFill: "rgba(220,220,220,0.75)",
                     highlightStroke: "rgba(220,220,220,1)",
-                    data: [65, 59, 80, 81, 56, 55, 40]
+                    data: []
                 },
                 {
-                    label: "My Second dataset",
+                    label: "Kills",
                     fillColor: "rgba(151,187,205,0.5)",
                     strokeColor: "rgba(151,187,205,0.8)",
                     highlightFill: "rgba(151,187,205,0.75)",
                     highlightStroke: "rgba(151,187,205,1)",
-                    data: [28, 48, 40, 19, 86, 27, 90]
+                    data: []
                 }
             ]
         },
             // Get the context of the canvas element we want to select
             ctx = document.getElementById("barchart").getContext("2d");
+
+        $.each(window.stats.history, function (key, val) {
+            data.labels.push(key);
+            data.datasets[0].data.push(val.saves);
+            data.datasets[1].data.push(val.kills);
+        });
+
         window.myBar = new Chart(ctx, {
             type: 'bar',
             data: data,
